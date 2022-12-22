1f10f118c571192e9f087ca7c1ba448de9959cb1
unknown <mike.magi@tptlive.ee>
2022-12-22 21:48:34 +0200

    done

---
+++ src/components/DoneComponent.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { useUserStore } from '@/stores/user';
+import { pocketBaseSymbol } from '@/symbols/injectionSymbols';
+import { inject, ref } from 'vue';
+import { useRouter } from 'vue-router';
+</script>
+
+<template>
+    <div>
+        <h1>
+        Done by Mike Mägi 22.12.2022 21:47
+        </h1>
+    </div>
+</template>
--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,6 +1,10 @@
 <script setup lang="ts">
 import { useUserStore } from '@/stores/user';
 import { pocketBaseSymbol } from '@/symbols/injectionSymbols';
-import { inject, onMounted, ref } from 'vue';
+import { inject, onMounted, ref } from 'vue'
+import DoneComponent from '@/components/DoneComponent.vue';
 </script>
 
+<template>
+  <DoneComponent />
+</template>
